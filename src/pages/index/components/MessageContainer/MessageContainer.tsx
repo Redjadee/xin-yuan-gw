343cad723ab2b57b9475e1b8c0aa793f1c3fe7c1
Redjade <jadered06@outlook.com>
2025-08-13 11:46:09 +0800

    chore: 配置、组成变动 - 将主页 Message 组件分离出来 - 更换 appid - 全局字体家族及其文件设置

---
+++ src/pages/index/components/MessageContainer/MessageContainer.tsx
@@ -0,0 +1,41 @@
+import { View, Text, Image } from '@tarojs/components'
+import './index.scss'
+
+interface MsgType {
+  profileHref: string //头像图片链接
+  name: string
+  content: string
+  time: string //~pending: 类型暂定
+}
+
+function Message({ profileHref, name, content, time }: MsgType) {
+  return (
+    <View className='msg'>
+      <Image src={profileHref} className='profile' />
+      <View className='middle-box'>
+        <Text className='name'>{name}</Text>
+        <Text className='content'>{content}</Text>
+      </View>
+      <Text className='time'>{time}</Text> 
+    </View>
+  )
+}
+
+export default function MessageContainer() {
+  const dataList: MsgType[] = [
+    {
+      profileHref: '', //~pending: 没加载出来图片 的防御性编程
+      name: '名字',
+      content: '内容', //~pending: 内容过长的逻辑
+      time: '刚刚'
+    }
+  ] //~pending: 请求数据
+  return (
+    <View>
+      {dataList.map((value, index) => (
+        <Message {...value} key={`home-msg-${index}`}/>
+      ))}
+    </View>
+  )
+}
+//
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text, Image, Swiper, SwiperItem } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
+import MessageContainer from './components/MessageContainer/MessageContainer'
 
 interface HeadType {
   type: '热门活动' | '我的消息'
 }
+
 function Head({ type }: HeadType) {
   type === '热门活动' ? 1 : 2 //~pending: 路由设置
 
@@ -28,46 +30,6 @@ function HotActivities() {
         </SwiperItem>
       ))}
     </Swiper>
-  )
-}
-//
-
-
-interface MsgType {
-  profileHref: string //头像图片链接
-  name: string
-  content: string
-  time: string //~pending: 类型暂定
-}
-
-function Message({ profileHref, name, content, time }: MsgType) {
-  return (
-    <View className='msg'>
-      <Image src={profileHref} className='profile' />
-      <View className='middle-box'>
-        <Text className='name'>{name}</Text>
-        <Text className='content'>{content}</Text>
-      </View>
-      <Text className='time'>{time}</Text> 
-    </View>
-  )
-}
-
-function MessageContainer() {
-  const dataList: MsgType[] = [
-    {
-      profileHref: '', //~pending: 没加载出来图片 的防御性编程
-      name: '名字',
-      content: '内容', //~pending: 内容过长的逻辑
-      time: '刚刚'
-    }
-  ] //~pending: 请求数据
-  return (
-    <View>
-      {dataList.map((value, index) => (
-        <Message {...value} key={`home-msg-${index}`}/>
-      ))}
-    </View>
   )
 }
 //
